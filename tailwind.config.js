3fd65aa0d44cdf13b984c298155a71258905f7da
S-A-Mahamud <sahariaalmahamudbd@gmail.com>
2026-01-20 22:06:01 +0600

    hero and footer section added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'hero-pattern': "url('/assets/bg.png')",
+//       },
+//     },
+//   },
+// }
+
+
+
